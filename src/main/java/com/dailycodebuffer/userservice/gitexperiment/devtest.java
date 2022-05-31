e72aab2486c1951db3596f4c53a6c87fd28805f0
praveennerji <praveennerji@gmail.com>
2022-06-01 00:45:04 +0530

    "second commit"

--- src/main/java/com/dailycodebuffer/userservice/gitexperiment/devtest.java
+++ src/main/java/com/dailycodebuffer/userservice/gitexperiment/devtest.java
@@ -3,5 +3,6 @@ package com.dailycodebuffer.userservice.gitexperiment;
 public class devtest {
     public static void main(String[] args) {
         System.out.println("javates");
+        //praveen added this
     }
 }
