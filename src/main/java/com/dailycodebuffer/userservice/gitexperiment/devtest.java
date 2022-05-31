950df2998a8ee5e7d9b5931763256788d704cfe0
praveennerji <praveennerji@gmail.com>
2022-06-01 00:33:30 +0530

    "first commit"

---
+++ src/main/java/com/dailycodebuffer/userservice/gitexperiment/devtest.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.userservice.gitexperiment;
+
+public class devtest {
+    public static void main(String[] args) {
+        System.out.println("javates");
+    }
+}
---
+++ src/main/java/com/dailycodebuffer/userservice/gitexperiment/javadevclass.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.userservice.gitexperiment;
+
+public class javadevclass {
+    public static void main(String[] args) {
+            System.out.println("javadev");
+    }
+}
